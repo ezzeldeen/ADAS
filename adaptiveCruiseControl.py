11a28189348c5970d858c2c53c529fb39e48d033
boogy <anehmr0@gmail.com>
2017-02-09 20:54:28 +0200

    algorithm pseudo code

--- adaptiveCruiseControl.py
+++ adaptiveCruiseControl.py
@@ -15,12 +15,27 @@ else:
 	print ('connection not successful')
 	#sys.exit('could no connect')
 
-errorCode,left_motor_handle=vrep.simxGetObjectHandle(clientID,'Pioneer_p3dx_leftMotor',vrep.simx_opmode_oneshot_wait)
-errorCode,right_motor_handle=vrep.simxGetObjectHandle(clientID,'Pioneer_p3dx_rightMotor',vrep.simx_opmode_oneshot_wait)
+errorCode,left_motor_handle=vrep.simxGetObjectHandle(clientID,'car1_leftMotor',vrep.simx_opmode_oneshot_wait)
+errorCode,right_motor_handle=vrep.simxGetObjectHandle(clientID,'car1_rightMotor',vrep.simx_opmode_oneshot_wait)
+
+
+desiredSpeed=5
+safetyDistance=1 # 1 meter
 
 # to make it steering 
-errorCode=vrep.simxSetJointTargetVelocity(clientID,left_motor_handle,4,vrep.simx_opmode_streaming)
-errorCode=vrep.simxSetJointTargetVelocity(clientID,right_motor_handle,4,vrep.simx_opmode_streaming)
+errorCode=vrep.simxSetJointTargetVelocity(clientID,left_motor_handle,desiredSpeed,vrep.simx_opmode_streaming)
+errorCode=vrep.simxSetJointTargetVelocity(clientID,right_motor_handle,desiredSpeed,vrep.simx_opmode_streaming)
 #errorCode=vrep.simxSetJointTargetVelocity(clientID,left_motor_handle,1.5,vrep.simx_opmode_streaming)
+## inifinty loop
+while 1 :
+    # DETERMINE distance bet. 2 cars -> call it diffDistance
+    # determine speed of the second car
+    ### case 1 : danger case
+    # if (diffDistance < safetyDistance && car2Speed < car1speed)
+    # then -> make decelerare car1
+    ### case 2 : not danger
+    # else if (diffDistance< safetyDistance && car2Speed > car1speed)
+    # then -> set car1Speed to desired speed
+    
 s = 2
 print s
