86f8a1872f01c006039b1b4ae5f00c1faa9f1fdb
MohamedA-M-Hassan <anehmr0@gmail.com>
2017-03-28 17:53:42 +0200

    stereoCalibrate

--- depth.py
+++ depth.py
@@ -1,7 +1,12 @@
 import numpy as np 
 import cv2
 from matplotlib import pyplot as plt
-
+"""
+img = cv2.imread('calib_radial.jpg',0)
+cv2.imshow('image',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+"""
 
 # termination criteria
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
@@ -12,7 +17,7 @@ objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
 
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
-imgpoints = [] # 2d points in image plane.
+imgpoints1 = [] # 2d points in image plane.
 
 img = cv2.imread('calib_radial.jpg')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -24,15 +29,20 @@ if ret == True:
     objpoints.append(objp)
 
     corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
-    imgpoints.append(corners2)
+    imgpoints1.append(corners2)
 
     # Draw and display the corners
     trial = cv2.drawChessboardCorners(img, (7,6), corners2,ret)
-    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
-    
-#cv2.destroyAllWindows()
+    rms, camera_matrix1, dist_coefs1, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints1, gray.shape[::-1],None,None)
 
+T=None
+R= None
+print (camera_matrix1)
+retval, cameraMatrix1, distCoeffs1, cameraMatrix2, distCoeffs2, R, T, E, F=cv2.stereoCalibrate(objpoints, imgpoints1, imgpoints1,   camera_matrix1, dist_coefs1, camera_matrix1, dist_coefs1,gray.shape[::-1], R, T, E=None, F=None, flags=cv2.CALIB_FIX_INTRINSIC,criteria=(cv2.TermCriteria_MAX_ITER+cv2.TermCriteria_EPS, 30, 1e-6) )
+print (cameraMatrix1)
+print (cameraMatrix2)
 
+print (R)
 """
 imgR = cv2.imread('1.jpg',0)
 imgL = cv2.imread('2.jpg',0)
