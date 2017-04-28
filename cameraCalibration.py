4d60f08d3a3133d84ecde03acde5fbf7b5641c1c
MohamedA-M-Hassan <anehmr0@gmail.com>
2017-04-28 23:30:53 +0200

    get the image from directory not from live streaming from camera

--- cameraCalibration.py
+++ cameraCalibration.py
@@ -1,56 +1,57 @@
 import numpy as np
 import cv2
 import glob
+import os
+import re
 
-# function parameter : 
+def cameraCalibration(path):
+  # termination criteria
+  criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
-#camPosition (string) = left or right
-#cameraNo : 0 or 1 or 2 
-def cameraCalibration(cameraNo,camPosition):    
-    # termination criteria
-    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+  # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+  objp = np.zeros((6*7,3), np.float32)
+  objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
 
-    # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-    objp = np.zeros((6*7,3), np.float32)
-    objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
+  # Arrays to store object points and image points from all the images.
+  objpoints = [] # 3d point in real world space
+  imgpoints = [] # 2d points in image plane.
 
-    # Arrays to store object points and image points from all the images.
-    objpoints = [] # 3d point in real world space
-    imgpoints = [] # 2d points in image plane.
+  directory = path
 
-    cap = cv2.VideoCapture(cameraNo)
-    if not cap.isOpened():
-        print ("Video device or file couldn't be opened")
-        exit()
+  def sort_humanly( l ):
+    """ Sort the given list in the way that humans expect.
+    """
+    convert = lambda text: int(text) if text.isdigit() else text
+    alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ]
+    l.sort( key=alphanum_key )
+    return l
+  images = sort_humanly(os.listdir(directory))
 
-    saveImgName = camPosition #string : left or right
-    saveImgNameVar=0
-    while(cap.isOpened()):
-        ret, img = cap.read()
-        gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-        cv2.imshow('frame',gray)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
 
-        # Find the chess board corners
-        ret, corners = cv2.findChessboardCorners(gray, (7,6),None)
+  for file_name in images:
+    name =directory + file_name
+    print(name)
+    img = cv2.imread(name)
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-        # If found, add object points, image points (after refining them)
-        if ret == True:
-            objpoints.append(objp)
+    # Find the chess board corners
+    ret, corners = cv2.findChessboardCorners(gray, (7,6),None)
 
-            cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
-            imgpoints.append(corners)
+    # If found, add object points, image points (after refining them)
+    if ret == True:
+      objpoints.append(objp)
 
-            # Draw and display the corners
-            saveImgNameVar +=1
-            cv2.imwrite(saveImgName+str(saveImgNameVar)+'.png',img)
-            cv2.drawChessboardCorners(img, (7,6), corners,ret)
-            cv2.imshow('img',img)
-            cv2.waitKey(500)
-        
-    ret, cameraMatrix, distortionCooeff, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
-    print ("cameraMat:", cameraMatrix)
-    print ("dist :", distortionCooeff)
-    cv2.destroyAllWindows()
-    return cameraMatrix , distortionCooeff , rvecs, tvecs , objpoints , imgpoints , gray.shape[::-1]
+      corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
+      imgpoints.append(corners2)
+
+      # Draw and display the corners
+      img = cv2.drawChessboardCorners(img, (7,6), corners2,ret)
+      cv2.imshow('img',img)
+      cv2.waitKey(500)
+
+  ret, cameraMatrix, distortionCooeff, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
+  cv2.destroyAllWindows()
+  return cameraMatrix,distortionCooeff, rvecs,tvecs,objpoints,imgpoints,gray.shape[::-1]
+
+
+#cameraMatrix2 , distortionCooeff2 , rvecs2, tvecs2 , objpoints2 , imgpoints1, shape = cameraCalibration('leftCamImg/')
