e58416c8d88b05b94a2ee514b8019a052e60acf9
MohamedA-M-Hassan <anehmr0@gmail.com>
2017-04-28 20:58:50 +0200

    the streaming from the cameras

--- cameraCalibration.py
+++ cameraCalibration.py
@@ -2,50 +2,55 @@ import numpy as np
 import cv2
 import glob
 
-# termination criteria
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+# function parameter : 
 
-# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((6*7,3), np.float32)
-objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
+#camPosition (string) = left or right
+#cameraNo : 0 or 1 or 2 
+def cameraCalibration(cameraNo,camPosition):    
+    # termination criteria
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
-# Arrays to store object points and image points from all the images.
-objpoints = [] # 3d point in real world space
-imgpoints = [] # 2d points in image plane.
+    # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+    objp = np.zeros((6*7,3), np.float32)
+    objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
 
-cap = cv2.VideoCapture(1)
-if not cap.isOpened():
-    print "Video device or file couldn't be opened"
-    exit()
+    # Arrays to store object points and image points from all the images.
+    objpoints = [] # 3d point in real world space
+    imgpoints = [] # 2d points in image plane.
 
-saveImgName = 'right'
-saveImgNameVar=0
-while(cap.isOpened()):
-    ret, img = cap.read()
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    cv2.imshow('frame',gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    cap = cv2.VideoCapture(cameraNo)
+    if not cap.isOpened():
+        print ("Video device or file couldn't be opened")
+        exit()
 
-    # Find the chess board corners
-    ret, corners = cv2.findChessboardCorners(gray, (7,6),None)
+    saveImgName = camPosition #string : left or right
+    saveImgNameVar=0
+    while(cap.isOpened()):
+        ret, img = cap.read()
+        gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        cv2.imshow('frame',gray)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-    # If found, add object points, image points (after refining them)
-    if ret == True:
-        objpoints.append(objp)
+        # Find the chess board corners
+        ret, corners = cv2.findChessboardCorners(gray, (7,6),None)
 
-        cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
-        imgpoints.append(corners)
+        # If found, add object points, image points (after refining them)
+        if ret == True:
+            objpoints.append(objp)
 
-        # Draw and display the corners
-        cv2.drawChessboardCorners(img, (7,6), corners,ret)
-        saveImgNameVar +=1
-        cv2.imwrite(saveImgName+str(saveImgNameVar)+'.png',img)
-        cv2.imshow('img',img)
-        cv2.waitKey(500)
-    
-ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
-print ("cameraMat:", mtx)
-print ("dist :", dist)
+            cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
+            imgpoints.append(corners)
 
-cv2.destroyAllWindows()
+            # Draw and display the corners
+            saveImgNameVar +=1
+            cv2.imwrite(saveImgName+str(saveImgNameVar)+'.png',img)
+            cv2.drawChessboardCorners(img, (7,6), corners,ret)
+            cv2.imshow('img',img)
+            cv2.waitKey(500)
+        
+    ret, cameraMatrix, distortionCooeff, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
+    print ("cameraMat:", cameraMatrix)
+    print ("dist :", distortionCooeff)
+    cv2.destroyAllWindows()
+    return cameraMatrix , distortionCooeff , rvecs, tvecs , objpoints , imgpoints , gray.shape[::-1]
