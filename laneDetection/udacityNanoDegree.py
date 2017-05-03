db9b83d6a23bf21d24f55e0e32e81be23d9b58c4
mohamed-mamdouh95 <1201270@eng.asu.edu.eg>
2017-05-03 14:36:06 +0200

    roi mask

--- laneDetection/udacityNanoDegree.py
+++ laneDetection/udacityNanoDegree.py
@@ -32,30 +32,37 @@ def filter_color(img):
 img = cv2.imread('images/carLane.jpg',1)
 
 #convert image to HSV
-HSV_img = cv2.cvtColor(img , cv2.COLOR_RGB2HSV)
+image = cv2.cvtColor(img , cv2.COLOR_RGB2HSV)
 #apply gaussian blur to remove noise
-blured_img = cv2.GaussianBlur(HSV_img,(3,3),0)
+image = cv2.GaussianBlur(image,(3,3),0)
 #filter image 
-filtered_img = filter_color(blured_img)
-sobelx = cv2.Sobel(gray_img,cv2.CV_64F,1,0,ksize=5)
-sobelx = sobelx.astype(np.uint8)
-ret,th1 = cv2.threshold(sobelx,30,150,cv2.THRESH_BINARY)
+image = filter_color(image)
+#apply canny edge detection
+image = cv2.Canny(image,30,130)
+#region of interest
+height,width=img.shape[:2]
+s = np.array([[[40,height],[width/2,height/2+40],[width/2,height/2 +40],[width-40,height]]],np.int32)
+image = region_of_interest(image,s)
+
+#sobelx = cv2.Sobel(gray_img,cv2.CV_64F,1,0,ksize=5)
+#sobelx = sobelx.astype(np.uint8)
+#ret,th1 = cv2.threshold(sobelx,30,150,cv2.THRESH_BINARY)
 
 #second path
-HLS_img = cv2.cvtColor(img , cv2.COLOR_RGB2HLS)
-s_channel = HLS_img[:,:,1]
-mask = cv2.inRange(s_channel,130,270)
-masked_image = cv2.bitwise_and(th1, mask)
-src = np.array([[340,280],[430,280],[670,410],[175,410]],np.float32)
-ss = np.array([[[340,280],[430,280],[670,410],[175,410]]],np.int32)
-dst = np.array([[150,0],[550,0],[550,630],[150,630]],np.float32)
-test = region_of_interest(masked_image,ss)
-M = cv2.getPerspectiveTransform(src, dst)
-warp = cv2.warpPerspective(test.copy(), M, (800, 600))
+#HLS_img = cv2.cvtColor(img , cv2.COLOR_RGB2HLS)
+#s_channel = HLS_img[:,:,1]
+#mask = cv2.inRange(s_channel,130,270)
+#masked_image = cv2.bitwise_and(th1, mask)
+#src = np.array([[340,280],[430,280],[670,410],[175,410]],np.float32)
+#ss = np.array([[[340,280],[430,280],[670,410],[175,410]]],np.int32)
+#dst = np.array([[150,0],[550,0],[550,630],[150,630]],np.float32)
+#test = region_of_interest(masked_image,ss)
+#M = cv2.getPerspectiveTransform(src, dst)
+#warp = cv2.warpPerspective(test.copy(), M, (800, 600))
 
-cv2.imshow("masked",test)
+cv2.imshow("masked",image)
 #cv2.imshow("original",masked_image)
-cv2.imshow("warp",warp)
+#cv2.imshow("warp",warp)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
