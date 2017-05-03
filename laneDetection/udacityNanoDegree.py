bef6922276734e1f2c81b335449d5ad4bcb16d74
mohamed-mamdouh95 <1201270@eng.asu.edu.eg>
2017-05-03 14:52:24 +0200

    draw fn

--- laneDetection/udacityNanoDegree.py
+++ laneDetection/udacityNanoDegree.py
@@ -1,6 +1,11 @@
 import cv2
 import numpy as np
 
+PREV_LEFT_X1 = None
+PREV_LEFT_X2 = None
+PREV_RIGHT_X1 = None
+PREV_RIGHT_X2 = None
+
 def region_of_interest(img, vertices):    
     #defining a blank mask to start with
     mask = np.zeros_like(img)       
@@ -28,7 +33,71 @@ def filter_color(img):
     img = cv2.bitwise_and(img, img, mask=cv2.bitwise_or(yellow_mask, white_mask))
     return img
 
-    
+def slope(line):
+    return (float(line[3]) - line[1]) / (float(line[2]) - line[0])
+
+
+def draw_lines(img, lines, color=[255, 0, 0], thickness=2):
+    global PREV_LEFT_X1, PREV_LEFT_X2, PREV_RIGHT_X1, PREV_RIGHT_X2
+    left_x = []
+    left_y = []
+    right_x = []
+    right_y = []
+
+    for line in lines:
+        line = line[0]
+        s = slope(line)
+
+        if 0.3 > s > -0.3:
+            continue
+
+        if s < 0:
+            if line[0] > img.shape[1] / 2 + 40:
+                continue
+
+            left_x += [line[0], line[2]]
+            left_y += [line[1], line[3]]
+            # cv2.line(img, (int(line[0]), int(line[1])), (int(line[2]), int(line[3])), [0, 0, 255], thickness)
+        else:
+            if line[0] < img.shape[1] / 2 - 40:
+                continue
+
+            right_x += [line[0], line[2]]
+            right_y += [line[1], line[3]]
+            # cv2.line(img, (int(line[0]), int(line[1])), (int(line[2]), int(line[3])), [255, 255, 0], thickness)
+
+    y1 = img.shape[0]
+    y2 = img.shape[0] / 2 + 90
+
+    if len(left_x) <= 1 or len(right_x) <= 1:
+        if PREV_LEFT_X1 is not None:
+            cv2.line(img, (int(PREV_LEFT_X1), int(y1)), (int(PREV_LEFT_X2), int(y2)), color, thickness)
+            cv2.line(img, (int(PREV_LEFT_X2), int(y1)), (int(PREV_RIGHT_X2), int(y2)), color, thickness)
+        return
+
+    left_poly = np.polynomial.Polynomial.fit(np.array(left_x), np.array(left_y), 1)
+    right_poly = np.polynomial.Polynomial.fit(np.array(right_x), np.array(right_y), 1)
+
+    left_x1 = (left_poly - y1).roots()
+    right_x1 = (right_poly - y1).roots()
+
+    left_x2 = (left_poly - y2).roots()
+    right_x2 = (right_poly - y2).roots()
+
+    if PREV_LEFT_X1 is not None:
+        left_x1 = PREV_LEFT_X1 * 0.7 + left_x1 * 0.3
+        left_x2 = PREV_LEFT_X2 * 0.7 + left_x2 * 0.3
+        right_x1 = PREV_RIGHT_X1 * 0.7 + right_x1 * 0.3
+        right_x2 = PREV_RIGHT_X2 * 0.7 + right_x2 * 0.3
+
+    PREV_LEFT_X1 = left_x1
+    PREV_LEFT_X2 = left_x2
+    PREV_RIGHT_X1 = right_x1
+    PREV_RIGHT_X2 = right_x2
+
+    cv2.line(img, (int(left_x1), int(y1)), (int(left_x2), int(y2)), color, thickness)
+    cv2.line(img, (int(right_x1), int(y1)), (int(right_x2), int(y2)), color, thickness)    
+
 img = cv2.imread('images/carLane.jpg',1)
 
 #convert image to HSV
