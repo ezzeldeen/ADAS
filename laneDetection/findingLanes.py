36cbf8d3291780caadbb924204fad2c6ee73bf02
mohamed-mamdouh95 <mm95mido@gmail.com>
2017-05-04 17:28:21 +0200

    solve right lane error

--- laneDetection/findingLanes.py
+++ laneDetection/findingLanes.py
@@ -6,7 +6,7 @@ def search_for_lane(img):
     histogram = np.sum(img[int( img.shape[0] / 2 ):, :],axis =0) 
     mid_point = np.int( histogram.shape[0]/2 )
     left_point = np.argmax(histogram[:mid_point])
-    right_point = np.argmax(histogram[mid_point:])
+    right_point = np.argmax(histogram[mid_point:]) + mid_point
     num_of_windows = 9 
     window_height = np.int(img.shape[0] / num_of_windows)
     nonzero = img.nonzero()
