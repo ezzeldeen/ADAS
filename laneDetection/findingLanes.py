8322caa8c1b3b89b3de675d5ad0dcbd964d0b7da
mohamed-mamdouh95 <mm95mido@gmail.com>
2017-05-04 13:03:51 +0200

    finishing up search for lane method

--- laneDetection/findingLanes.py
+++ laneDetection/findingLanes.py
@@ -27,9 +27,29 @@ def search_for_lane(img):
         win_x_right_high = current_right + margin
         cv2.rectangle(out_img,(win_x_left_low,win_y_low),(win_x_left_high,win_y_high) ,(0,255,0),2)
         cv2.rectangle(out_img,(win_x_right_low,win_y_low),(win_x_right_high,win_y_high) ,(0,0,255),2)
-        cv2.imshow("test",img)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+        good_left_lane = (( nonzeroy >= win_y_low ) & (nonzeroy < win_y_high ) & 
+                            ( nonzerox >= win_x_left_low) & (nonzerox < win_x_left_high )).nonzero()[0]
+        good_right_lane = (( nonzeroy >= win_y_low ) & (nonzeroy < win_y_high ) & 
+                            ( nonzerox >= win_x_right_low) & (nonzerox < win_x_right_high )).nonzero()[0]
+        left_lane.append(good_left_lane)
+        right_lane.append(good_right_lane)
+        if len(good_left_lane) > minpix :
+            current_left = np.int(np.mean(nonzerox[good_left_lane]))                    
+        if len(good_right_lane) > minpix :
+            current_right = np.int(np.mean(nonzerox[good_right_lane]))
+    left_lane = np.concatenate(left_lane)
+    right_lane = np.concatenate(right_lane)
+    leftx = nonzerox[left_lane]
+    rightx= nonzerox[right_lane]
+    lefty = nonzeroy[left_lane]
+    righty = nonzeroy[right_lane]
+    left_fit = np.polyfit(lefty,leftx,2)
+    right_fit = np.poly(righty,rightx,2)
+
+    return leftx,left_fit,rightx,right_fit    
+        
+                    
+                            
     return
 
    
