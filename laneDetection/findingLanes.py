a4c493d81068e3279ea2bc7e4a42d448031219f3
mohamed-mamdouh95 <mm95mido@gmail.com>
2017-05-04 12:44:05 +0200

    sliding over lanes

--- laneDetection/findingLanes.py
+++ laneDetection/findingLanes.py
@@ -8,4 +8,23 @@ def search_for_lane(img):
     right_point = np.argmax(histogram[mid_point:])
     num_of_windows = 9 
     window_height = np.int(img.shape[0] / num_of_windows)
+    nonzero = img.nonzero()
+    nonzeroy = np.array(nonzero[0])
+    nonzerox = np.array(nonzero[1])
+    current_right = right_point 
+    current_left = left_point
+    margin = 100 
+    minpix = 50 
+    left_lane = []
+    right_lane = []
+    for window in range(0,num_of_windows):
+        win_y_low = img.shape[0] - ((window +1 ) * window_height) 
+        win_y_high = win_y_low + window_height 
+        win_x_left_low = current_left - margin
+        win_x_left_high = current_left + margin        
+        win_x_right_low = current_right -  margin        
+        win_x_right_high = current_right + margin
+        cv2.rectangle(img,(win_x_left_low,win_y_low),(win_x_left_high,win_y_high) ,(0,0,255),2)
+        cv2.rectangle(img,(win_x_right_low,win_y_low),(win_x_right_high,win_y_high) ,(0,0,255),2)
+        
     
