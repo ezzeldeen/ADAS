05ecc7516b67249599d0ed0e3048f9f0a42fd1ff
mohamed-mamdouh95 <mm95mido@gmail.com>
2017-05-04 13:48:10 +0200

    refine code

--- laneDetection/advancedLaneDetetion.py
+++ laneDetection/advancedLaneDetetion.py
@@ -1,28 +1,8 @@
 import cv2
 import numpy as np
 import findingLanes as fl
+import drawLanes as dr
 
-PREV_LEFT_X1 = None
-PREV_LEFT_X2 = None
-PREV_RIGHT_X1 = None
-PREV_RIGHT_X2 = None
-
-def region_of_interest(img, vertices):    
-    #defining a blank mask to start with
-    mask = np.zeros_like(img)       
-    #defining a 3 channel or 1 channel color to fill the mask with depending on the input image
-    if len(img.shape) > 2:
-        channel_count = img.shape[2]  # i.e. 3 or 4 depending on your image
-        ignore_mask_color = (255,) * channel_count
-    else:
-        ignore_mask_color = 255
-        
-    #filling pixels inside the polygon defined by "vertices" with the fill color    
-    cv2.fillPoly(mask, vertices, ignore_mask_color)
-    
-    #returning the image only where mask pixels are nonzero
-    masked_img = cv2.bitwise_and(img, mask)
-    return masked_img
 
 def filter_color(img):
     yellow_min = np.array([65, 80, 80], np.uint8)
@@ -65,67 +45,6 @@ def slope(line):
     return (float(line[3]) - line[1]) / (float(line[2]) - line[0])
 
 
-def draw_lines(img, lines, color=[255, 0, 0], thickness=2):
-    global PREV_LEFT_X1, PREV_LEFT_X2, PREV_RIGHT_X1, PREV_RIGHT_X2
-    left_x = []
-    left_y = []
-    right_x = []
-    right_y = []
-
-    for line in lines:
-        line = line[0]
-        s = slope(line)
-
-        if 0.3 > s > -0.3:
-            continue
-
-        if s < 0:
-            if line[0] > img.shape[1] / 2 + 40:
-                continue
-
-            left_x += [line[0], line[2]]
-            left_y += [line[1], line[3]]
-            # cv2.line(img, (int(line[0]), int(line[1])), (int(line[2]), int(line[3])), [0, 0, 255], thickness)
-        else:
-            if line[0] < img.shape[1] / 2 - 40:
-                continue
-
-            right_x += [line[0], line[2]]
-            right_y += [line[1], line[3]]
-            # cv2.line(img, (int(line[0]), int(line[1])), (int(line[2]), int(line[3])), [255, 255, 0], thickness)
-
-    y1 = img.shape[0]
-    y2 = img.shape[0] / 2 + 90
-
-    if len(left_x) <= 1 or len(right_x) <= 1:
-        if PREV_LEFT_X1 is not None:
-            cv2.line(img, (int(PREV_LEFT_X1), int(y1)), (int(PREV_LEFT_X2), int(y2)), color, thickness)
-            cv2.line(img, (int(PREV_LEFT_X2), int(y1)), (int(PREV_RIGHT_X2), int(y2)), color, thickness)
-        return
-
-    left_poly = np.polynomial.Polynomial.fit(np.array(left_x), np.array(left_y), 1)
-    right_poly = np.polynomial.Polynomial.fit(np.array(right_x), np.array(right_y), 1)
-
-    left_x1 = (left_poly - y1).roots()
-    right_x1 = (right_poly - y1).roots()
-
-    left_x2 = (left_poly - y2).roots()
-    right_x2 = (right_poly - y2).roots()
-
-    if PREV_LEFT_X1 is not None:
-        left_x1 = PREV_LEFT_X1 * 0.7 + left_x1 * 0.3
-        left_x2 = PREV_LEFT_X2 * 0.7 + left_x2 * 0.3
-        right_x1 = PREV_RIGHT_X1 * 0.7 + right_x1 * 0.3
-        right_x2 = PREV_RIGHT_X2 * 0.7 + right_x2 * 0.3
-
-    PREV_LEFT_X1 = left_x1
-    PREV_LEFT_X2 = left_x2
-    PREV_RIGHT_X1 = right_x1
-    PREV_RIGHT_X2 = right_x2
-
-    cv2.line(img, (int(left_x1), int(y1)), (int(left_x2), int(y2)), color, thickness)
-    cv2.line(img, (int(right_x1), int(y1)), (int(right_x2), int(y2)), color, thickness)    
-
 def weighted_img(img, initial_img, alpha=1., bita=1., gamma=0.):
     return cv2.addWeighted(initial_img, alpha, img,bita,gamma)
 
@@ -151,10 +70,10 @@ src = np.float32([[430, 330],[520, 330],[900, 540],[150, 540]])
 dst = np.float32([[150, 0],[900, 0], [900, 540], [150, 540]])
 warped = warp_image(combined,src,dst)
 unwarped = unwarp_image(warped,src,dst)
-
-fl.search_for_lane(warped)
-cv2.imshow("out",warped)
-cv2.imshow("rr",unwarped)
+lx,lxf,rx,rxf =fl.search_for_lane(warped)
+o = dr.draw(img,lx,lxf,rx,rxf,src,dst)
+cv2.imshow("out",o)
+#cv2.imshow("rr",unwarped)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
