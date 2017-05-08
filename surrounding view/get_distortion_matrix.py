99fd59f16feafe859c6156dcede9c832bb79ace6
Ezz Eldeen <ezzahmed.360@gmail.com>
2017-05-08 04:15:34 +0200

    fisheye distortion correction

---
+++ surrounding view/get_distortion_matrix.py
@@ -0,0 +1,57 @@
+import numpy as np
+import cv2
+
+USAGE = '''
+USAGE: calib.py [--save <filename>] [--debug <output path>] [--square_size] [<image mask>]
+'''
+
+if __name__ == '__main__':
+    import sys, getopt
+    from glob import glob
+
+    args, img_mask = getopt.getopt(sys.argv[1:], '', ['save=', 'debug=', 'square_size='])
+    args = dict(args)
+    try: img_mask = img_mask[0]
+    except: img_mask = 'distorted/*.jpg'
+    img_names = glob(img_mask)
+    debug_dir = args.get('--debug')
+    square_size = float(args.get('--square_size', 1.0))
+
+    pattern_size = (9, 6)
+    pattern_points = np.zeros( (np.prod(pattern_size), 3), np.float32 )
+    pattern_points[:,:2] = np.indices(pattern_size).T.reshape(-1, 2)
+    pattern_points *= square_size
+
+    obj_points = []
+    img_points = []
+    h, w = 0, 0
+    for fn in img_names:
+        print 'processing %s...' % fn,
+        img = cv2.imread(fn, 0)
+        img2 = img
+        h, w = img.shape[:2]
+        found, corners = cv2.findChessboardCorners(img, pattern_size)
+        if found:
+            term = ( cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_COUNT, 30, 0.1 )
+            cv2.cornerSubPix(img, corners, (5, 5), (-1, -1), term)
+        if debug_dir:
+            vis = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+            #cv2.drawChessboardCorners(vis, pattern_size, corners, found)
+        if not found:
+            print 'chessboard not found'
+            continue
+        
+        img_points.append(corners.reshape(-1, 2))
+        obj_points.append(pattern_points)
+        
+        cv2.drawChessboardCorners(img, pattern_size, corners, found)
+        rms, camera_matrix, dist_coefs, rvecs, tvecs = cv2.calibrateCamera(obj_points, img_points, (w, h))
+        dst = cv2.undistort(cv2.imread(fn, 0), camera_matrix, dist_coefs, None )
+              
+        #cv2.imwrite('undistorted/2.png',dst) 
+        #cv2.imwrite("draw_chess_board/1.jpg" ,img)
+                
+        cv2.waitKey(500)
+
+        print 'ok'
+        cv2.destroyAllWindows()
