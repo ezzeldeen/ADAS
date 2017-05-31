9135e95ac4594bdbc7b5e291baf090242a448c0f
mohamed-mamdouh95 <mm95mido@gmail.com>
2017-05-31 16:27:45 +0000

    getting opencv c++ working!

--- laneDetection/advancedLaneDetection.cpp
+++ laneDetection/advancedLaneDetection.cpp
@@ -1,4 +1,23 @@
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "stdlib.h"
+#include "stdio.h"
+#include <iostream>
 
-using namespace std;
-void main() {
+using namespace cv ;
+
+int main()
+{
+    Mat img ;
+    img = imread("images.jpg");
+    if(! img.data )                              // Check for invalid input
+    {
+        std::cout <<  "Could not open or find the image" << std::endl ;
+        return -1;
+    }
+
+    namedWindow( "test", CV_WINDOW_AUTOSIZE );
+    imshow ("test",img);
+    waitKey(0);
+    return 0;
 }
